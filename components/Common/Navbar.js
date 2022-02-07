fdc5a1502342496ebd64c86ea615dad80733e233
HuakFreeTime <komsark_@hotmail.co.th>
2022-02-07 23:00:20 +0700

    fix: connect-css

--- components/Common/Navbar.js
+++ components/Common/Navbar.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
 import Web3 from "web3";
+import styles from "../../styles/Navbar.module.css";
 export default function navbar() {
   const { authenticate, isAuthenticated, logout } = useMoralis();
   const [walletAddress, setWalletAddress] = useState();
   const getAddressWallet = async () => {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    const web3 = new Web3(
+      Web3.givenProvider ||
+        "https://ropsten.infura.io/v3/b0f95459c5a149cc9032a56d32fd1bdf"
+    );
     const accounts = await web3.eth.requestAccounts();
     setWalletAddress(accounts[0]);
   };
@@ -14,62 +18,23 @@ export default function navbar() {
   }, []);
   return (
     <div>
-      <div className="topnav" style={{ marginLeft: "15%" }}>
+      <div className={styles.topnav} style={{ marginLeft: "15%" }}>
         <h1>FARM SOUVIRNIORS</h1>
-
         {isAuthenticated ? (
           <div>
-            <p style={{ color: "white" }}>Connected to Address {walletAddress}</p>
-            <div onClick={() => logout()} className="buttonLogin">
+            <p style={{ color: "white" }}>
+              Connected to Address {walletAddress}
+            </p>
+            <div onClick={() => logout()} className={styles.buttonLogin}>
               <p>logout </p>
             </div>
           </div>
         ) : (
-          <div onClick={() => authenticate()} className="buttonLogin">
+          <div onClick={() => authenticate()} className={styles.buttonLogin}>
             <p>Login</p>
           </div>
         )}
       </div>
-
-      <style jsx>
-        {`
-          .buttonLogin {
-            height: 50%;
-            width: 20%;
-            background-color: red;
-            margin-left: 100px;
-          }
-          .topnav {
-            overflow: hidden;
-            background-color: #272c32;
-            display: flex;
-            flex-direction: row;
-          }
-
-          .topnav h1 {
-            float: left;
-            color: #ffffff;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            font-family: "Prompt", sans-serif;
-            font-style: normal;
-            font-weight: bold;
-            font-size: 24px;
-            line-height: 36px;
-          }
-
-          .topnav h1:hover {
-            background-color: #272c32;
-            color: #ffffff;
-          }
-
-          .topnav h1.active {
-            background-color: #04aa6d;
-            color: white;
-          }
-        `}
-      </style>
     </div>
   );
 }
--- pages/PlayGame.js
+++ pages/PlayGame.js
@@ -1,7 +1,9 @@
-import React from 'react';
-
+import React from "react";
+import styles from "../styles/Playgame.module.css";
 export default function PlayGame() {
-  return <div style={{ marginLeft: "15%" }}>
-      <h1>PlayGame</h1>
-  </div>;
+  return (
+    <div style={{ marginLeft: "15%" }}>
+      <h1 className={styles.title}>PlayGame</h1>
+    </div>
+  );
 }
