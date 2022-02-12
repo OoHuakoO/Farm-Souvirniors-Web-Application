f09c1e6fa49cc5f93b8335d238810f20f6ef3b7c
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-02-12 22:06:02 +0700

    Link

--- components/Common/Sidebar.js
+++ components/Common/Sidebar.js
@@ -1,38 +1,43 @@
 import React from "react";
 import styles from "../../styles/Sidebar.module.css";
-import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SubMenu,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent
-} from "react-pro-sidebar";
+import Link from "next/link";
 
 export default function Sidebar() {
   return (
     <div>
       <div className={styles.sidenav}>
-        <a href="/PlayGame">
-          <div className={styles.menuplaygame}>
-            <span className="material-icons">sports_esports</span>
-            <span> Play Game</span>
-          </div>
-        </a>
-        <a href="/Marketplace">
-        <div className={styles.menuplaygame}>
-          <span className="material-icons">shopping_bag</span>
-          <span>Marketplace</span>
-          </div>
-        </a>
-        <a href="/MyItem">
-        <div className={styles.menuplaygame}>
-          <span className="material-icons">person</span>
-          <span>My Item</span>
-          </div>
-        </a>
-        
+        <Link href="/PlayGame">
+          <a>
+            <div className={styles.menuplaygame}>
+              <span className="material-icons">sports_esports</span>
+              <span> Play Game</span>
+            </div>
+          </a>
+        </Link>
+        <Link href="/Marketplace">
+          <a>
+            <div className={styles.menuplaygame}>
+              <span className="material-icons">shopping_bag</span>
+              <span>Marketplace</span>
+            </div>
+          </a>
+        </Link>
+        <Link href="/MyItem">
+          <a>
+            <div className={styles.menuplaygame}>
+              <span className="material-icons">person</span>
+              <span>My Item</span>
+            </div>
+          </a>
+        </Link>
+        <Link href="/Sell">
+          <a>
+            <div className={styles.menuplaygame}>
+              <span className="material-icons">sell</span>
+              <span>Sell</span>
+            </div>
+          </a>
+        </Link>
       </div>
     </div>
   );
