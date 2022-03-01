2501cceea060d16c629d816a16506e928bc27058
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-03-01 18:02:53 +0700

    Exchange

--- components/CardExchange.js
+++ components/CardExchange.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../styles/Exchange.module.css";
 
-const CardExchange = () => {
+const CardExchange = (props) => {
   return <div>CardExchange</div>;
 };
 
--- pages/Exchange.js
+++ pages/Exchange.js
@@ -1,5 +1,47 @@
 import React from "react";
+import CardExchange from "../components/CardExchange";
+import styles from "../styles/Exchange.module.css";
 
 export default function Exchange() {
-  return <div>Exchange</div>;
+  const cardExchange = [
+    {
+      name: "Corn",
+      UID: 10024510303,
+      image: "Corn.png",
+      category: "Vetgetable",
+    },
+    {
+      name: "Corn",
+      UID: 10024510303,
+      image: "Corn.png",
+      category: "Animal",
+    },
+    {
+      name: "Corn",
+      UID: 100245103035,
+      image: "Corn.png",
+      category: "Fruit",
+    },
+    {
+      name: "Corn",
+      UID: 10024510303,
+      image: "Corn.png",
+      category: "Vetgetable",
+    },
+   
+  ];
+  return (
+    <div className={styles.mainMyItem}>
+      {
+       cardExchange.map((item,index) => {
+          return (
+            <CardExchange key={index} {...item} />
+          
+          )
+        }
+        )
+      }
+      
+    </div>
+  );
 }
