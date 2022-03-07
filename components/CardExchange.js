e80c67b3988c3a7830a270de82e6c6badccc5d5e
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-03-08 00:52:29 +0700

    exchange

--- components/CardExchange.js
+++ components/CardExchange.js
@@ -8,7 +8,7 @@ const CardExchange = () => {
       <div className={styles.TopicExchange}>Swap</div>
       <div className={styles.exchangeCoin}>
         <div className={styles.exchangeCoin1}>
-          <div>Swap</div>
+          <div className={styles.Coin1}>555</div>
           <div>
             <select
               className={[styles.formSelect, "form-select"].join(" ")}
@@ -24,16 +24,9 @@ const CardExchange = () => {
           <span className="material-icons">south</span>
         </div>
         <div className={styles.exchangeCoin2}>
-          <div>Swap</div>
-          <div>
-            <select
-              className={[styles.formSelect, "form-select"].join(" ")}
-              aria-label="Default select example"
-            >
-              <option value="1">Fruit</option>
-              <option value="2">wood</option>
-              <option value="3">Meat</option>
-            </select>
+          <div className={styles.Coin1}>2.2563</div>
+          <div className={styles.coin2}>
+            CoinFruit
           </div>
         </div>
       </div>
