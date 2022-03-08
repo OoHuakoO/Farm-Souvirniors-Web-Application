82b0257cda5154906b4753f42484f5075908cfa5
HuakFreeTime <komsark_@hotmail.co.th>
2022-03-08 22:21:59 +0700

    fix: change-to-ganache

--- web3/index.js
+++ web3/index.js
@@ -4,11 +4,11 @@ let RandomBox;
 import dataFromNFT from "../build/contracts/NFT.json";
 import dataRandomBox from "../build/contracts/RandomBox.json";
 // Development
-// const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
 // Testnet Ropsten
-const web3 = new Web3(
-  Web3.givenProvider || "https://data-seed-prebsc-1-s1.binance.org:8545"
-);
+// const web3 = new Web3(
+//   Web3.givenProvider || "https://data-seed-prebsc-1-s1.binance.org:8545"
+// );
 const getContract = async () => {
   const netId = await web3.eth.net.getId();
 
