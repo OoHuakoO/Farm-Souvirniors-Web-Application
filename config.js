50351dc3de02686668c69e5d9e3e01979f73c4d2
HuakFreeTime <komsark_@hotmail.co.th>
2022-03-09 00:07:03 +0700

    feat: add-webTestnetBaseUrl

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 export default {
   apiBaseURL: "https://farm-souvirniors-api.herokuapp.com",
-  webBaseUrl: "https://farm-souvirniors.herokuapp.com/",
+  webLocalBaseUrl: "https://farm-souvirniors.herokuapp.com/",
+  webTestnetBeseUrl : "https://farm-souvirniors-testnet.herokuapp.com/",
   web3ProviderGanache: "http://127.0.0.1:7545",
   web3ProviderBscTestnet: "https://data-seed-prebsc-1-s1.binance.org:8545",
+
 };
