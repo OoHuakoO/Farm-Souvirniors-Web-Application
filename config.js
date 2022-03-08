47a582f2e44130434a255fc1345aa703072db604
HuakFreeTime <komsark_@hotmail.co.th>
2022-03-09 00:43:45 +0700

    refactor: edit-config-file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
   apiBaseURL: "https://farm-souvirniors-api.herokuapp.com",
-  webLocalBaseUrl: "https://farm-souvirniors.herokuapp.com/",
-  webTestnetBeseUrl : "https://farm-souvirniors-testnet.herokuapp.com/",
+  webTestnetBeseUrl: "https://farm-souvirniors.herokuapp.com/",
+  webLocalBeseUrl : "https://farm-souvirniors-local.herokuapp.com/",
   web3ProviderGanache: "http://127.0.0.1:7545",
   web3ProviderBscTestnet: "https://data-seed-prebsc-1-s1.binance.org:8545",
 };
