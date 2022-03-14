7a0fba4d438cf8a4cc5dff630d3285595e23766f
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-03-15 01:52:51 +0700

    css

--- pages/Exchange.js
+++ pages/Exchange.js
@@ -1,41 +1,40 @@
 import React, { useState } from "react";
-
 import { Deposit } from "../components/Deposit";
 import { Withdraw } from "../components/Withdraw";
 import styles from "../styles/Exchange.module.css";
 
-
-
-
 export default function Exchange() {
-  const cardExchange = [
-    {
-      name: "Corn",
-      UID: 10024510303,
-      image: "Corn.png",
-      category: "Deposit",
-    },
-    
-  ];
   const [CurrentTab, setCurrentTab] = useState(1);
   const changeTab = (index) => {
-    setCurrentTab(index)
-  }
-
+    setCurrentTab(index);
+  };
   const SwapTab = () => {
-    if(CurrentTab==0) {
-      return <Deposit/>
+    if (CurrentTab == 0) {
+      return <Deposit />;
+    } else if (CurrentTab == 1) {
+      return <Withdraw />;
     }
-    else if (CurrentTab==1) {
-      return <Withdraw/>
-    }
-  } 
+  };
   return (
     <div>
-      <button className={CurrentTab === 0 ? styles.buttonCategoryActive : styles.buttonCategory} onClick={()=>changeTab(0)} >Deposit</button>
-      <button className={CurrentTab === 1 ? styles.buttonCategoryActive : styles.buttonCategory} onClick={()=>changeTab(1)} >withdraw</button>
-     <SwapTab/> 
-      
+    <div className={styles.topicSelecyExchage}>
+      <button
+        className={
+          CurrentTab === 0 ? styles.buttonCategoryActive : styles.buttonCategory
+        }
+        onClick={() => changeTab(0)}
+      >
+        Deposit
+      </button>
+      <button
+        className={
+          CurrentTab === 1 ? styles.buttonCategoryActive : styles.buttonCategory
+        }
+        onClick={() => changeTab(1)}
+      >
+        withdraw
+      </button></div>
+      <SwapTab />
     </div>
   );
 }
