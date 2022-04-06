dfa44d6fab94f88e5970c2d5cf2c6818e2357157
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-04-07 01:01:27 +0700

    จัด หน้า detail กล่องสุ่ม

--- components/ModalDetailNFT.js
+++ components/ModalDetailNFT.js
@@ -33,95 +33,95 @@ const ModalDetailNFT = (props) => {
         <div className={styles.detailPopup}>
           {props.item.name === "Animal Chests" ? (
             <div>
-              <div> Random Items</div>
-              <div>
+              <div className={styles.randomItems}> Random Items</div>
+              <div className={styles.detailRandomItems}>
                 <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302011/Farm-Souvirniors/Cow_vpruov.png"
                   alt="Cow"
                   width={40}
                   height={40}
-                />
-                <span>10% Chance</span>
+                />&nbsp;
+                <div>10% Chance</div>
               </div>
-              <div>
+              <div className={styles.detailRandomItems}>
                 <Image
                   src='https://res.cloudinary.com/smilejob/image/upload/v1644302011/Farm-Souvirniors/Pig_gqadmy.png'
                   alt="Pig"
                   width={40}
                   height={40}
-                />
-                <span>30% Chance</span>
+                />&nbsp;
+                <div>30% Chance</div>
               </div>
-              <div>
+              <div className={styles.detailRandomItems}>
                 <Image
                   src='https://res.cloudinary.com/smilejob/image/upload/v1646201434/Farm-Souvirniors/bird.png'
                   alt="Bird"
                   width={40}
                   height={40}
-                />
-                <span>60% Chance</span>
+                />&nbsp;
+                <div>60% Chance</div>
               </div>
             </div>
           ) : props.item.name === "Fruit Chests" ? (
             <div>
-              <div> Random Items</div>
-              <div>
+              <div className={styles.randomItems}> Random Items</div>
+              <div className={styles.detailRandomItems}>
                 <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302036/Farm-Souvirniors/Strawberry_vw8bde.png"
                   alt="Strawberry"
                   width={40}
                   height={40}
-                />
-                <span>10% Chance</span>
+                />&nbsp;
+                <div>10% Chance</div>
               </div>
-              <div>
+              <div className={styles.detailRandomItems}>
                 <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302035/Farm-Souvirniors/Grape_xljcuk.png"
                   alt="Grape"
                   width={40}
                   height={40}
-                />
-                <span>30% Chance</span>
+                />&nbsp;
+                <div>30% Chance</div>
               </div>
-              <div>
+              <div className={styles.detailRandomItems}>
                 <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302034/Farm-Souvirniors/Apple_tpsbss.png"
                   alt="Apple"
                   width={40}
                   height={40}
-                />
-                <span>60% Chance</span>
+                />&nbsp;
+                <div>60% Chance</div>
               </div>
             </div>
           ) : props.item.name === "Vegetable Chests" ? (
             <div>
-              <div> Random Items</div>
-              <div>
+              <div className={styles.randomItems}> Random Items</div>
+              <div className={styles.detailRandomItems}>
               <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302036/Farm-Souvirniors/Pumpkin_uov1l1.png"
                   alt="Pumpkin"
                   width={40}
                   height={40}
-                />
-                <span>10% Chance</span>
+                />&nbsp;
+                <div>10% Chance</div>
               </div>
-              <div>
+              <div className={styles.detailRandomItems}>
               <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302035/Farm-Souvirniors/Cauliflower_gwjecd.png"
                   alt="Cauliflower"
                   width={40}
                   height={40}
-                />
-                <span>30% Chance</span>
+                />&nbsp;
+                <div>30% Chance</div>
               </div>
-              <div>
+              <div className={styles.detailRandomItems}>
               <Image
                   src="https://res.cloudinary.com/smilejob/image/upload/v1644302035/Farm-Souvirniors/Corn_dl4f2a.png"
                   alt="Corn"
                   width={40}
                   height={40}
-                />
-                <span>60% Chance</span>
+                />&nbsp;
+                <div>60% Chance</div>
               </div>
             </div>
           ) : (
