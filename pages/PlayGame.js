63ad14df063125363574d0d7aaf49d5a4fa6a5f5
HuakFreeTime <komsark_@hotmail.co.th>
2022-04-08 23:52:01 +0700

    fix:send-wallet-to-webGL

--- pages/PlayGame.js
+++ pages/PlayGame.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/Playgame.module.css";
-import { useMoralis } from "react-moralis";
-import { useUserState } from "../context/user";
+import Web3 from "web3";
 import Unity, { UnityContext } from "react-unity-webgl";
 export default function PlayGame() {
-  const { isAuthenticated } = useMoralis();
-  const { share_address_wallet } = useUserState();
   const unityContext = new UnityContext({
-    loaderUrl: "buildUnity/Other.loader.js",
-    dataUrl: "buildUnity/Other.data",
-    frameworkUrl: "buildUnity/Other.framework.js",
-    codeUrl: "buildUnity/Other.wasm",
-    webglContextAttributes: {
-      preserveDrawingBuffer: true,
-    },
+    loaderUrl: "buildUnity/Huak.loader.js",
+    dataUrl: "buildUnity/Huak.data",
+    frameworkUrl: "buildUnity/Huak.framework.js",
+    codeUrl: "buildUnity/Huak.wasm",
   });
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const web3 = new Web3(Web3.givenProvider || Config.web3ProviderGanache);
+      const accounts = await web3.eth.requestAccounts();
+      unityContext.send("GameManager", "SpawnEnemies", accounts[0]);
+    }, 6000);
+  }, []);
+
   return (
-<div style={{ marginLeft: "15%" }}>
-<h1 className={styles.title}>PlayGame</h1>
+    <div style={{ marginLeft: "15%" }}>
+      <h1 className={styles.title}>PlayGame</h1>
       <Unity
         style={{ height: "680px", width: "700px", alignSelf: "center" }}
         unityContext={unityContext}
