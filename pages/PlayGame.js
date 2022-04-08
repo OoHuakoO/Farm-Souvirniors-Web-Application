7f79db450e6ce487fc259ca493ca56c77780f3ca
HuakFreeTime <komsark_@hotmail.co.th>
2022-04-08 13:50:47 +0700

    feat: add-react-webgl

--- pages/PlayGame.js
+++ pages/PlayGame.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Playgame.module.css";
+import { useMoralis } from "react-moralis";
+import { useUserState } from "../context/user";
 export default function PlayGame() {
+  const { isAuthenticated } = useMoralis();
+  const { share_address_wallet } = useUserState();
   return (
     <div style={{ marginLeft: "15%" }}>
       <h1 className={styles.title}>PlayGame</h1>
