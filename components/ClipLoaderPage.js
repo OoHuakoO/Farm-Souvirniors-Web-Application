f13eb5cba9f3b06f3522b81f69056a0842571ab9
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-04-17 17:48:30 +0700

    ClipLoaderButton css

--- components/ClipLoaderPage.js
+++ components/ClipLoaderPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { css } from "@emotion/react";
 import ClipLoader from "react-spinners/ClipLoader";
+import styles from "../styles/MyItem.module.css";
 const override = css`
-  display: block;
-  margin: 0px;
+  display: flex;
+  margin: 0px auto;
+  
+  
 `;
 const ClipLoaderButton = ({ loading, color }) => {
   return (
-    <ClipLoader color={color} loading={loading} size={120} css={override} />
+    <div className={styles.clipLoaderButton}>
+    <ClipLoader color={color} loading={loading} size={120} css={override} /></div>
   );
 };
 export default ClipLoaderButton;
