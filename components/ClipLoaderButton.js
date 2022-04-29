7b1787713d6bb55f94da474ea3a693d89950a2ab
June-25431 <80663999+June-25431@users.noreply.github.com>
2022-04-29 23:15:50 +0700

    ...

--- components/ClipLoaderButton.js
+++ components/ClipLoaderButton.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { css } from "@emotion/react";
 import ClipLoader from "react-spinners/ClipLoader";
+
 const override = css`
   display: block;
   margin: 0px;
 `;
 const ClipLoaderButton = ({ loading, color }) => {
   return (
+    
     <ClipLoader color={color} loading={loading} size={20} css={override} />
   );
 };
